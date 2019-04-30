e23a84afd1bf3d3efafe9756637c660ee3a609d1
jvc5546 <jvc5546@gmail.com>
2019-04-30 17:55:50 -0500

    Completed all promise tests

--- exercises/bare_minimum/basicChaining.js
+++ exercises/bare_minimum/basicChaining.js
@@ -9,13 +9,22 @@
  */
 
 var fs = require('fs');
-var request = require('request');
+var promises = require('./promisification.js'); //We can pull the github method to pull user data
 var Promise = require('bluebird');
-var db = Promise.promisifyAll(fs);
+var fsFunctions = Promise.promisifyAll(fs);
+
 
 
 var fetchProfileAndWriteToFile = function(readFilePath, writeFilePath) {
   // TODO
+  return fsFunctions.readFileAsync(readFilePath) //Reads file for username
+        .then(function(data){
+          var user = data.toString().split('\n')[0];
+          return promises.getGitHubProfileAsync(user); //that retrieves the profile
+        })
+        .then(function(userProfile){
+          return fsFunctions.writeFileAsync(writeFilePath,JSON.stringify(userProfile));  //writing the profile to file
+        });
 };
 
 // Export these functions so we can test them
